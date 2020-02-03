bfd7b54e8bb2f8064a46ad18b369ee1ff08a900f
nunes <fernando.nunes@dp6.com.br>
2020-02-03 13:45:24 -0300

    Added a License and updated the setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-   name='marketing_attribution_models',
-   version='1.0',
-   description='Metodos de atribuicao de midia',
-   author='Andre Tocci',
-   author_email='andre.tocci@dp6.com.br',
-   packages=['marketing_attribution_models'],  #same as name
-   install_requires=['numpy', 'pandas','matplotlib', 'seaborn', ], #external packages as dependencies
+    name='marketing_attribution_models',
+    version='1.0',
+    description='Metodos de atribuicao de midia',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Andre Tocci',
+    author_email='andre.tocci@dp6.com.br',
+    url='https://github.com/DP6/Marketing-Attribution-Models',
+    packages=['marketing_attribution_models'],
+    install_requires=['numpy', 'pandas','matplotlib', 'seaborn', ],
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
 )
