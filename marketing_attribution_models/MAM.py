0acd3375efe94e5ac3e4088a029f742d7aebd24e
Luan Gabriel Silva Fernandes <luan.fernandes@buser.com.br>
2022-04-29 19:14:43 -0300

    sort is actually used

--- marketing_attribution_models/MAM.py
+++ marketing_attribution_models/MAM.py
@@ -309,7 +309,7 @@ class MAM:
             self.print("group_channels == True")
             self.print("Grouping channels...")
             temp_channels = (
-                df.groupby(group_channels_by_id_list, sort=False)[channels_colname]
+                df.groupby(group_channels_by_id_list)[channels_colname]
                 .apply(list)
                 .reset_index()
             )
@@ -334,7 +334,7 @@ class MAM:
                 #     df[session_id_col].isin(valid_sessions)
                 # ]
                 temp_journey_id_conv = (
-                    df_temp.groupby(group_channels_by_id_list, sort=False)[
+                    df_temp.groupby(group_channels_by_id_list)[
                         journey_with_conv_colname
                     ]
                     .max()
