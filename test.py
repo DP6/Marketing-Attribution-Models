db57f6dcd0a25b7b20e533a0d341851902b7d78d
Luan Gabriel Silva Fernandes <luan.fernandes@buser.com.br>
2022-05-06 12:25:52 -0300

    test and notebook changes

--- test.py
+++ test.py
@@ -5,14 +5,35 @@ import seaborn as sns
 
 from marketing_attribution_models import MAM
 
+np.random.seed(42)
+
 pd.set_option("display.float_format", lambda x: "%.3f" % x)
 pd.set_option("max_colwidth", None)
 
-df = pd.read_csv("/home/luanfernandes/Downloads/sessions.csv")
+users = [
+    # "1472526294.1645135142",
+    "1317354717.1641321280",
+    # "679601293.1636465111"
+]
+
+df = pd.read_csv(
+    "/home/luanfernandes/Downloads/sessions-full.csv",
+    dtype={
+        "user_pseudo_id": str,
+        "session_id": str,
+        "user_id": str,
+        "is_conversion": bool,
+        "source_medium": str,
+        "partition_date": str,
+    },
+    parse_dates=True,
+)
+df = df[df.user_pseudo_id.isin(users)].sample(frac=1.0)
+
 df = df.drop(
     columns=[
-        "page_referrer",
-        "page_location",
+        # "page_referrer",
+        # "page_location",
         "utm_source",
         "utm_medium",
         "utm_campaign",
@@ -39,3 +60,8 @@ DP_tribution = MAM(
 DP_tribution.as_pd_dataframe()
 
 DP_tribution.attribution_last_click()
+DP_tribution.attribution_time_decay()
+
+print(DP_tribution.DataFrame)
+
+print(DP_tribution.original_df)
