0e5b0fbb7c6e8a8b0533f06adb8fe302d9c0710c
Luan Gabriel Silva Fernandes <luan.fernandes@buser.com.br>
2022-05-18 11:31:56 -0300

    add more testes

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -1,7 +1,8 @@
 import pytest
+from typing import Tuple, Any
 
 import pandas as pd
-from pandas.testing import assert_series_equal
+from pandas.testing import assert_series_equal, assert_frame_equal
 import numpy as np
 
 from marketing_attribution_models import MAM
@@ -97,4 +98,122 @@ def test_tim_decay(model_fixture):
                 ],
             }
         ),
+    )
+
+
+def test_markov_transition_matrix(model_fixture):
+    model: MAM = model_fixture()
+    result: Tuple[Any] = model.attribution_markov(transition_to_same_state=True)
+    assert_frame_equal(
+        result[2],
+        pd.DataFrame.from_dict(
+            {
+                "(inicio)": {
+                    "(inicio)": 0.0,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.0,
+                    "google_ads": 0.0,
+                    "(null)": 0.0,
+                    "(conversion)": 0.0,
+                },
+                "seo": {
+                    "(inicio)": 0.4,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.1,
+                    "google_ads": 0.0,
+                    "(null)": 0.0,
+                    "(conversion)": 0.0,
+                },
+                "crm": {
+                    "(inicio)": 0.0,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.1,
+                    "google_ads": 0.0,
+                    "(null)": 0.0,
+                    "(conversion)": 0.0,
+                },
+                "direct": {
+                    "(inicio)": 0.6,
+                    "seo": 1.0,
+                    "crm": 1.0,
+                    "direct": 0.2,
+                    "google_ads": 1.0,
+                    "(null)": 0.0,
+                    "(conversion)": 0.0,
+                },
+                "google_ads": {
+                    "(inicio)": 0.0,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.1,
+                    "google_ads": 0.0,
+                    "(null)": 0.0,
+                    "(conversion)": 0.0,
+                },
+                "(null)": {
+                    "(inicio)": 0.0,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.1,
+                    "google_ads": 0.0,
+                    "(null)": 1.0,
+                    "(conversion)": 0.0,
+                },
+                "(conversion)": {
+                    "(inicio)": 0.0,
+                    "seo": 0.0,
+                    "crm": 0.0,
+                    "direct": 0.4,
+                    "google_ads": 0.0,
+                    "(null)": 0.0,
+                    "(conversion)": 1.0,
+                },
+            }
+        ),
+        check_like=True,
+    )
+
+
+def test_markov_removal_effect(model_fixture):
+    model: MAM = model_fixture()
+    result: Tuple[Any] = model.attribution_markov(transition_to_same_state=True)
+    assert_frame_equal(
+        result[3],
+        pd.DataFrame.from_dict(
+            data={
+                "removal_effect": {
+                    "crm": 0.1666666666666653,
+                    "google_ads": 0.1666666666666653,
+                    "seo": 0.49999999999999933,
+                    "direct": 1.0,
+                }
+            }
+        ),
+        check_like=True,
+    )
+
+
+def test_markov_journeys_weights(model_fixture):
+    model: MAM = model_fixture()
+    result: pd.Series = model.attribution_markov(transition_to_same_state=True)
+    assert_series_equal(
+        result[0],
+        pd.Series(
+            {
+                0: [0.4615384615384619, 0.07692307692307636, 0.4615384615384619],
+                1: [0.3333333333333331, 0.666666666666667],
+                2: [0.4615384615384619, 0.07692307692307636, 0.4615384615384619],
+                3: [0.19999999999999982, 0.40000000000000013, 0.40000000000000013],
+                4: [
+                    0.28571428571428575,
+                    0.28571428571428575,
+                    0.14285714285714268,
+                    0.28571428571428575,
+                ],
+            }
+        ),
+        check_names=False,
     )
