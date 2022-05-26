c77f66b9ca7fd1d98998084cb4627f8a690190d2
Luan Gabriel Silva Fernandes <luan.fernandes@buser.com.br>
2022-05-26 17:49:01 -0300

    generalize conv col in markov

--- marketing_attribution_models/MAM.py
+++ marketing_attribution_models/MAM.py
@@ -94,6 +94,7 @@ class MAM:
         self.sep = path_separator
         self.group_by_channels_models = None
         self.round_values_to = round_values_to
+        self.journey_with_conv_colname = journey_with_conv_colname
 
         ##########################################################
         ################## Instance attributes ###################
@@ -1370,7 +1371,7 @@ class MAM:
         chmap = {a: b[0] for a, b in zip(frame.index.values, frame.values)}
         _df = pd.concat([self.channels, self.conversion_value], axis=1)
         channels_value = _df.apply(
-            lambda row: [chmap[x] * row.is_conversion for x in row.source_medium],
+            lambda row: [chmap[x] * row[self.journey_with_conv_colname] for x in row.source_medium],
             axis=1,
         )
         channels_value = channels_value.apply(lambda x: list(np.array(x) / sum(x)))
